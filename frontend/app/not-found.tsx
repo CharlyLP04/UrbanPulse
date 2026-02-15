a6357b73bd1f7924653747c766cf9df18dee83cb
Breyan21 <bryan.sebas.matias2001@gmail.com>
2026-02-15 14:54:59 -0600

    creacion de pantalla de error 500

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>404 - Página no encontrada</h1>
+      <p>La página que buscas no existe.</p>
+    </div>
+  );
+}
